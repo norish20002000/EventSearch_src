f19eb0a3ebbda0619f1ed9f30c948a63dae77d99
shduo <norish20002000@gmail.com>
2020-06-05 17:39:28 +0900

    add top logo, and mod css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,11 +46,12 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img class='top_logo' src="/image/top/logo.jpg">
+                    {{-- {{ config('app.name', 'Laravel') }} --}}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> --}}
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
