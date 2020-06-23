0102c28d63c7f24ebc4ae21e7cf085308bdc1a45
shduo <norish20002000@gmail.com>
2020-06-23 20:00:48 +0900

    mod top to TOP

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -22,7 +22,7 @@
 <div class="footer">
     <div class="copywrite">
         <div>
-            <a href="/">Top</a> ｜ <a href="{{route('request')}}">情報掲載申し込み</a> ｜ <a href="{{route('company')}}">運営会社</a>
+            <a href="/">TOP</a> ｜ <a href="{{route('request')}}">情報掲載申し込み</a> ｜ <a href="{{route('company')}}">運営会社</a>
         </div>
         <div>
             <p>Copyright© 2020 EventBank Corporation. All Rights Reserved.</p>
