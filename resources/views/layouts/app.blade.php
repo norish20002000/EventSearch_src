753881ccc09b33706ead14a1699ccd4f823c5117
shduo <norish20002000@gmail.com>
2020-06-24 16:55:27 +0900

    add right side of navbar menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,9 +50,9 @@
                     <img class='top_logo' src="/image/top/logo.jpg">
                     {{-- {{ config('app.name', 'Laravel') }} --}}
                 </a>
-                {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button> --}}
+                </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
@@ -62,6 +62,12 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('company')}}">会社概要</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('request')}}">情報掲載申し込み</a>
+                        </li>
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
