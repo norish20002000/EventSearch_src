a118e9294ce42059fc5e422446947e2d2e1cb1f1
shduo <norish20002000@gmail.com>
2022-10-05 18:21:16 +0900

    add index.html

--- routes/web.php
+++ routes/web.php
@@ -22,25 +22,23 @@ Route::get('/', 'WelcomeController@index');
 
 // Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/mail', 'TestMailController@send');
+// Route::get('/mail', 'TestMailController@send');
 
-Route::get('todayevent', 'WelcomeController@searchToday')->name('todayevent');
-Route::get('tomorrowevent', 'WelcomeController@searchTomorrow')->name('tomorrowevent');
-Route::get('weekendevent', 'WelcomeController@searchWeekend')->name('weekendevent');
-// Route::get('request', 'WelcomeController@request')->name('request');
-Route::get('company', 'WelcomeController@company')->name('company');
-// Route::get('alliance', 'WelcomeController@alliance')->name('alliance');
-Route::get('eventdetail/{id}', 'EventController@show')->name('eventdetail');
-Route::get('eventgenre/{genre_id}', 'EventController@genre')->name('eventgenre');
+// Route::get('todayevent', 'WelcomeController@searchToday')->name('todayevent');
+// Route::get('tomorrowevent', 'WelcomeController@searchTomorrow')->name('tomorrowevent');
+// Route::get('weekendevent', 'WelcomeController@searchWeekend')->name('weekendevent');
+// Route::get('company', 'WelcomeController@company')->name('company');
+// Route::get('eventdetail/{id}', 'EventController@show')->name('eventdetail');
+// Route::get('eventgenre/{genre_id}', 'EventController@genre')->name('eventgenre');
 
-Route::get('eventbank/event/edit/{id?}', 'EventBankController@edit')->name('eventedit');
-Route::post('/eventbank/event/register', 'EventBankController@register')->name('eventregister');
-Route::get('/eventbank/event/ope', 'EventBankController@showList')->name('eventopelist');
-Route::get('/eventbank/event/export', 'EventBankController@exportCsv')->name('eventexport');
-Route::get('/eventbank/event/export/csv', 'EventBankController@exportCsvList')->name('eventexportlist');
+// Route::get('eventbank/event/edit/{id?}', 'EventBankController@edit')->name('eventedit');
+// Route::post('/eventbank/event/register', 'EventBankController@register')->name('eventregister');
+// Route::get('/eventbank/event/ope', 'EventBankController@showList')->name('eventopelist');
+// Route::get('/eventbank/event/export', 'EventBankController@exportCsv')->name('eventexport');
+// Route::get('/eventbank/event/export/csv', 'EventBankController@exportCsvList')->name('eventexportlist');
 
 
-Route::get('debug', 'EventBankController@getData');
+// Route::get('debug', 'EventBankController@getData');
 
-// debub
-Route::get('/debug', 'EventBankController@getDataBySearch');
+// // debub
+// Route::get('/debug', 'EventBankController@getDataBySearch');
